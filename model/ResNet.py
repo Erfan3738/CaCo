7071ef0cb3234f4eb40f889501beb827a03db1c8
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-10 10:13:45 -0600

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -741,8 +741,8 @@ class PyramidNet(nn.Module):
 
         super(PyramidNet, self).__init__()
 
-        self.alpha = 200
-        self.depth = 50
+        self.alpha = 48
+        self.depth = 110
         self.inplanes = 16
         
         if block == Bottleneck1:
