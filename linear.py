4dbdf606cda8ff2322a0586348dd7cf1c69f4166
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-15 22:12:21 +0330

    Update linear.py

--- linear.py
+++ linear.py
@@ -447,19 +447,7 @@ def accuracy(output, target, topk=(1,)):
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
-# utils
-@torch.no_grad()
-def concat_all_gather(tensor):
-    """
-    Performs all_gather operation on the provided tensors.
-    *** Warning ***: torch.distributed.all_gather has no gradient.
-    """
-    tensors_gather = [torch.ones_like(tensor)
-                      for _ in range(torch.distributed.get_world_size())]
-    torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
 
-    output = torch.cat(tensors_gather, dim=0)
-    return output
 
 if __name__ == '__main__':
     main()
