c09d6e7aea36120f1ca3f6ed0eea6a96863a8218
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-15 16:58:22 -0600

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -123,8 +123,8 @@ def main_worker(args):
     # Data loading code
     if args.dataset=='stl10':
         #traindir = os.path.join(args.data, 'train')
-        normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                                     std=[0.2023, 0.1994, 0.2010])
+        normalize = transforms.Normalize(mean=[0.4465, 0.4402, 0.4039],
+                                     std=[0.2608, 0.2571, 0.2708])
         if args.multi_crop:
             from data_processing.MultiCrop_Transform import Multi_Transform
             multi_transform = Multi_Transform([32, 24],
@@ -136,33 +136,34 @@ def main_worker(args):
         else:
 
             augmentation1 = transforms.Compose([
-                    transforms.RandomResizedCrop(32),
+                    transforms.RandomResizedCrop(96),
                     transforms.RandomHorizontalFlip(p=0.5),
                     
                     transforms.RandomApply([
-                        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)  # not strengthened
+                        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)  # not strengthened
                     ], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
-                    #transforms.GaussianBlur(kernel_size=5),
+                    transforms.GaussianBlur(kernel_size=9),
                     transforms.ToTensor(),
                     normalize
                 ])
 
             augmentation2 = transforms.Compose([
-                    transforms.RandomResizedCrop(32),
+                    transforms.RandomResizedCrop(96),
                     transforms.RandomHorizontalFlip(p=0.5),
                     
                     transforms.RandomApply([
-                        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)  # not strengthened
+                        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1)  # not strengthened
                     ], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
-                   # transforms.GaussianBlur(kernel_size=5),
+                   transforms.GaussianBlur(kernel_size=9),
                     transforms.ToTensor(),
                     normalize
                 ])
             
             
-            train_dataset = CIFAR10(root='./datasets', train=True, download=True, transform=TwoCropsTransform2(augmentation1, augmentation2))
+            #train_dataset = CIFAR10(root='./datasets', train=True, download=True, transform=TwoCropsTransform2(augmentation1, augmentation2))
+            train_dataset = STL10(root='./data', split='unlabeled', download=True, transform=TwoCropsTransform2(augmentation1, augmentation2))
             
         testdir = os.path.join(args.data, 'val')
         transform_test = transforms.Compose([
@@ -172,8 +173,10 @@ def main_worker(args):
             normalize,
         ])
         from data_processing.imagenet import imagenet
-        val_dataset =CIFAR10(root='./datasets', train=True, download=True, transform=transform_test)
-        test_dataset =CIFAR10(root='./datasets', train=False, download=True, transform=transform_test)
+        #val_dataset =CIFAR10(root='./datasets', train=True, download=True, transform=transform_test)
+        val_dataset = STL10(root='./data', split='train', download=True, transform=transform_test)
+        #test_dataset =CIFAR10(root='./datasets', train=False, download=True, transform=transform_test)
+        test_dataset = STL10(root='./data', split='test', download=True, transform=transform_test)
 
     else:
         print("We only support ImageNet dataset currently")
