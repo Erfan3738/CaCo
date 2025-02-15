15fc37bee7e85dbe1a69ba024347654d26e50cdb
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-15 13:58:26 -0600

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -295,7 +295,7 @@ class ResNet(nn.Module):
         groups: int = 1,
         width_per_group: int = 64,
         replace_stride_with_dilation: Optional[List[bool]] = None,
-        norm_layer: Optional[Callable[..., nn.Module]] = None
+        norm_layer: Optional[Callable[..., nn.Module]] = norm_layer
     ) -> None:
         super(ResNet, self).__init__()
         if norm_layer is None:
