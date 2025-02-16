9a9125990dd5431ce0e52b61b5cbed7c2aa57b6d
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-15 18:50:32 -0600

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -25,8 +25,8 @@ class CaCo(nn.Module):
         dim_mlp = self.encoder_q.fc.weight.shape[1]
         print("dim_mlp:",dim_mlp)
         # we do not keep 
-        self.encoder_q.fc = self._build_mlp(3,dim_mlp,args.mlp_dim,dim,last_bn=False)
-        self.encoder_k.fc = self._build_mlp(3,dim_mlp,args.mlp_dim,dim,last_bn=False)
+        self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=False)
+        self.encoder_k.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=False)
         
         #self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=True)
         #self.encoder_k.fc = self._build_mlp(2, dim_mlp, args.mlp_dim, dim, last_bn=True)
