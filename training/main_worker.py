4aba48be572627abcad3bc6e2cd91a9bc2605d34
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-18 01:39:28 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -163,7 +163,7 @@ def main_worker(args):
             
             #train_dataset = CIFAR10(root='./datasets', train=True, download=True, transform=TwoCropsTransform2(augmentation1, augmentation2))
             #train_dataset = STL10(root='./data', split='unlabeled', download=True, transform=TwoCropsTransform2(augmentation1, augmentation2))
-            train_dataset = Imagenette(root =  './data', split= 'train', size= '320px', download=True, transform =TwoCropsTransform2(augmentation1, augmentation2))
+            train_dataset = Imagenette(root =  './data', split= 'train', size= 'full', download=True, transform =TwoCropsTransform2(augmentation1, augmentation2))
             
         testdir = os.path.join(args.data, 'val')
         transform_test = transforms.Compose([
@@ -176,11 +176,11 @@ def main_worker(args):
         from data_processing.imagenet import imagenet
         #val_dataset =CIFAR10(root='./datasets', train=True, download=True, transform=transform_test)
         #val_dataset = STL10(root='./data', split='train', download=True, transform=transform_test)
-        val_dataset= Imagenette(root =  './data/val', split= 'train', size= '320px', download=True, transform =transform_test)
+        val_dataset= Imagenette(root =  './data/val', split= 'train', size= 'full', download=True, transform =transform_test)
         
         #test_dataset =CIFAR10(root='./datasets', train=False, download=True, transform=transform_test)
         #test_dataset = STL10(root='./data', split='test', download=True, transform=transform_test
-        test_dataset = Imagenette(root =  './data/test', split= 'val', size= '320px', download=True, transform =transform_test)
+        test_dataset = Imagenette(root =  './data/test', split= 'val', size= 'full', download=True, transform =transform_test)
 
     else:
         print("We only support ImageNet dataset currently")
