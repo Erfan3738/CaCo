924aafc2e1cc9280acf839badcaa7500c84f161d
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-25 18:34:36 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -63,8 +63,8 @@ class CaCo(nn.Module):
         dim_mlp = self.encoder_q.fc.weight.shape[1]
         print("dim_mlp:",dim_mlp)
         # we do not keep 
-        self.encoder_q.fc = self._build_mlp1(3,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=False, num_splits=8)
-        self.encoder_k.fc = self._build_mlp1(3,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=False, num_splits=8)
+        self.encoder_q.fc = self._build_mlp1(3,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=True, num_splits=8)
+        self.encoder_k.fc = self._build_mlp1(3,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=True, num_splits=8)
         
         #self.encoder_q.fc = self._build_mlp(2,dim_mlp,args.mlp_dim,dim,last_bn=True)
         #self.encoder_k.fc = self._build_mlp(2, dim_mlp, args.mlp_dim, dim, last_bn=True)
@@ -76,7 +76,7 @@ class CaCo(nn.Module):
 
         self.K=args.cluster
 
-    def _build_mlp1(self, num_layers, input_dim, mlp_dim, output_dim, last_bn=True, use_split_bn=False, num_splits=2):
+    def _build_mlp1(self, num_layers, input_dim, mlp_dim, output_dim, last_bn=True, use_split_bn=False, num_splits=8):
         mlp = []
         for l in range(num_layers):
             dim1 = input_dim if l == 0 else mlp_dim
@@ -163,16 +163,16 @@ class CaCo(nn.Module):
                 # if update_key_encoder:
             self._momentum_update_key_encoder_param(moco_momentum)# update the key encoder
            
-            #im_q_, idx_unshuffle = self._batch_shuffle_single_gpu(im_q)
-            q = self.encoder_k(im_q, use_feature=False)  # keys: NxC
+            im_q_, idx_unshuffle = self._batch_shuffle_single_gpu(im_q)
+            q = self.encoder_k(im_q_, use_feature=False)  # keys: NxC
             q = nn.functional.normalize(q, dim=1)
-            #q = self._batch_unshuffle_single_gpu(q, idx_unshuffle)
+            q = self._batch_unshuffle_single_gpu(q, idx_unshuffle)
             q = q.detach()
 
-            #im_k_, idx_unshuffle1 = self._batch_shuffle_single_gpu(im_k)
-            k = self.encoder_k(im_k, use_feature=False)  # keys: NxC
+            im_k_, idx_unshuffle1 = self._batch_shuffle_single_gpu(im_k)
+            k = self.encoder_k(im_k_, use_feature=False)  # keys: NxC
             k = nn.functional.normalize(k, dim=1)
-            #k = self._batch_unshuffle_single_gpu(k, idx_unshuffle1)
+            k = self._batch_unshuffle_single_gpu(k, idx_unshuffle1)
             k = k.detach()
         return q_pred, k_pred, q, k
     def forward_withoutpred_multicrop(self,im_q_list,im_k,moco_momentum):
