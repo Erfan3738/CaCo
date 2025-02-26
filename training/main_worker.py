6055daeef4fdd8d614f26230d0c80d644be69712
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-27 00:55:18 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -82,9 +82,9 @@ def main_worker(args):
     from model.optimizer import  AdamW
     from model.optimizer import  LARS
     #optimizer = AdamW(model.parameters(), init_lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=args.weight_decay)
-    #optimizer = LARS(model.parameters(), args.lr ,weight_decay=args.weight_decay,momentum=args.momentum)
+    optimizer = LARS(model.parameters(), args.lr ,weight_decay=args.weight_decay,momentum=args.momentum)
     
-    optimizer = torch.optim.SGD(model.parameters(), args.lr, args.weight_decay, args.momentum)
+    #optimizer = torch.optim.SGD(model.parameters(), args.lr, args.weight_decay, args.momentum)
 
 
     model.cuda()
@@ -207,8 +207,8 @@ def main_worker(args):
     best_Acc=0
     for epoch in range(args.start_epoch, args.epochs):
 
-        adjust_learning_rate(optimizer, epoch, args)
-        #adjust_learning_rate2(optimizer, epoch, args, args.lr)    
+        #adjust_learning_rate(optimizer, epoch, args)
+        adjust_learning_rate2(optimizer, epoch, args, args.lr)    
         #if args.type<10:
         if args.moco_m_decay:
             moco_momentum = adjust_moco_momentum(epoch, args)
