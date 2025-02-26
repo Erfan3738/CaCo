3d854c692e541cf2fa2faf1c0fdbbbb106e7cb37
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-27 00:11:27 +0330

    Update train_utils.py

--- training/train_utils.py
+++ training/train_utils.py
@@ -72,7 +72,9 @@ def adjust_learning_rate(optimizer, epoch, args):
     """Decay the learning rate based on schedule"""
     lr = args.lr
     if args.cos==1:  # cosine lr schedule
-        lr *= 0.5 * (1. + math.cos(math.pi * epoch / args.epochs))
+        lr = args.lr_final + 0.5 * (1. + math.cos(math.pi * epoch / args.epochs)) * (args.lr - args.lr_final)
+    elif args.cos==2:
+        lr*=math.cos(math.pi * epoch / (args.epochs*2))
     else:  # stepwise lr schedule
         for milestone in args.schedule:
             lr *= 0.1 if epoch >= milestone else 1.
