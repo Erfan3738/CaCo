1ee883832e96ac130ea469d3487dac17f2eb355a
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-27 00:12:19 +0330

    Update main_worker.py

--- training/main_worker.py
+++ training/main_worker.py
@@ -137,7 +137,7 @@ def main_worker(args):
             augmentation1 = transforms.Compose([
                     transforms.RandomResizedCrop(32,scale=(0.08, 1.0)),
                     transforms.RandomApply([
-                        transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)  # not strengthened
+                        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
                     ], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
                     #transforms.RandomApply([GaussianBlur([.1, 2.])], p=1.0),
@@ -150,7 +150,7 @@ def main_worker(args):
             augmentation2 = transforms.Compose([
                     transforms.RandomResizedCrop(32,scale=(0.08, 1.0)),
                     transforms.RandomApply([
-                        transforms.ColorJitter(0.4, 0.4, 0.2, 0.1)  # not strengthened
+                        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
                     ], p=0.8),
                     transforms.RandomGrayscale(p=0.2),
                     #transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.1),
