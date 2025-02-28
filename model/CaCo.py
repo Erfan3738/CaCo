bfe2acba810b86b1c47d1c68d7ee9e11182f6b8f
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-02-28 15:54:56 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -97,7 +97,7 @@ class CaCo(nn.Module):
         print("dim_mlp:",dim_mlp)
         # we do not keep 
         self.encoder_q.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
-        self.encoder_k.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_q.fc)
+        self.encoder_k.fc = nn.Sequential(nn.Linear(dim_mlp, dim_mlp), nn.ReLU(), self.encoder_k.fc)
         #self.encoder_q.fc = self._build_mlp1(2,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=True, num_splits=8)
         #self.encoder_k.fc = self._build_mlp1(2,dim_mlp,args.mlp_dim,dim,last_bn=False, use_split_bn=True, num_splits=8)
         
