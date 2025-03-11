c91059f43c2e0727859ea8b51c11cc42b06635b9
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-03-11 18:03:53 +0330

    Update CaCo.py

--- model/CaCo.py
+++ model/CaCo.py
@@ -233,16 +233,16 @@ class CaCo(nn.Module):
             
             self._momentum_update_key_encoder_param(moco_momentum)# update the key encoder
            
-            im_q_, idx_unshuffle = self._batch_shuffle_single_gpu(im_q)
-            q = self.encoder_k(im_q_)  # keys: NxC
+            #im_q_, idx_unshuffle = self._batch_shuffle_single_gpu(im_q)
+            q = self.encoder_k(im_q)  # keys: NxC
             q = nn.functional.normalize(q, dim=1)
-            q = self._batch_unshuffle_single_gpu(q, idx_unshuffle)
+            #q = self._batch_unshuffle_single_gpu(q, idx_unshuffle)
             q = q.detach()
 
-            im_k_, idx_unshuffle = self._batch_shuffle_single_gpu(im_k)
-            k = self.encoder_k(im_k_)  # keys: NxC
+            #im_k_, idx_unshuffle = self._batch_shuffle_single_gpu(im_k)
+            k = self.encoder_k(im_k)  # keys: NxC
             k = nn.functional.normalize(k, dim=1)
-            k = self._batch_unshuffle_single_gpu(k, idx_unshuffle)
+            #k = self._batch_unshuffle_single_gpu(k, idx_unshuffle)
             k = k.detach()
         return q_pred, k_pred, q, k
     def forward_withoutpred_multicrop(self,im_q_list,im_k,moco_momentum):
