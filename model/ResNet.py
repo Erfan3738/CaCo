7abdb1feb522afcf5daa879ef0d1b45c1e2f5175
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-03-16 13:08:45 +0330

    Update ResNet.py

--- model/ResNet.py
+++ model/ResNet.py
@@ -70,7 +70,7 @@ class BasicBlock(nn.Module):
         groups: int = 1,
         base_width: int = 64,
         dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None
+        norm_layer: Optional[Callable[..., nn.Module]] = norm_layer
     ) -> None:
         super(BasicBlock, self).__init__()
         if norm_layer is None:
@@ -295,7 +295,7 @@ class ResNet(nn.Module):
         groups: int = 1,
         width_per_group: int = 64,
         replace_stride_with_dilation: Optional[List[bool]] = None,
-        norm_layer: Optional[Callable[..., nn.Module]] = None 
+        norm_layer: Optional[Callable[..., nn.Module]] = norm_layer 
     ) -> None:
         super(ResNet, self).__init__()
         if norm_layer is None:
@@ -331,7 +331,7 @@ class ResNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
+            elif isinstance(m, SplitBatchNorm):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
