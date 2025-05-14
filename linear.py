57fb6f953183572b8e42a8778b324b45e3c3a267
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-05-15 00:02:23 +0330

    Update linear.py

--- linear.py
+++ linear.py
@@ -127,9 +127,9 @@ def main_worker(args):
             state_dict = checkpoint['state_dict']
             for k in list(state_dict.keys()):
                 # retain only encoder_q up to before the embedding layer
-                if k.startswith('encoder_q') and not k.startswith('encoder_q.fc'):
+                if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
                     # remove prefix
-                    state_dict[k[len("encoder_q."):]] = state_dict[k]
+                    state_dict[k[len("module.encoder_q."):]] = state_dict[k]
                     del state_dict[k]
             args.start_epoch = 0
             msg = model.load_state_dict(state_dict, strict=False)
