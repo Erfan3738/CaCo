6f740a55c9aa8f98f2ed5ae3ef829385d134188f
Erfan3738 <138443555+Erfan3738@users.noreply.github.com>
2025-05-16 02:54:51 +0330

    Update linear.py

--- linear.py
+++ linear.py
@@ -55,7 +55,7 @@ parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial (base) learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--wd', '--weight-decay', default=5e-4, type=float,
+parser.add_argument('--wd', '--weight-decay', default= 1e-4, type=float,
                     metavar='W', help='weight decay (default: 0.)',
                     dest='weight_decay')
 parser.add_argument('-p', '--print-freq', default=10, type=int,
